6265ce10e0fcab04acfd4c25d39c25c6c7288ed7
abulbashar98 <abulbashar98116@gmail.com>
2022-03-17 16:46:47 +0600

    creating-components-using-ArrayOf-Object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,155 @@ import logo from './logo.svg';
 import './App.css';
 
 
+// Part 4
+
+/*-------------------------------------------------------- ----------------*\
+Create Dynamic data Components using An Array of Object... In 3 steps.....
+\*---------------------------------------------------------- -----------------*/
+
+function App() {
+
+  // 1st way of adding making props and data dynamic... Not recommended...
+  /*
+    const besties = ['nabil', 'tushar', 'tamjid', 'shahid', 'sifat'];
+  
+    return (
+      <div className="App">
+        <ul>
+          <li>{besties[0]}</li>
+          <li>{besties[1]}</li>
+          <li>{besties[2]}</li>
+          <li>{besties[3]}</li>
+          <li>{besties[4]}</li>
+        </ul>
+        <Friend name={besties[0]} age="29" profession="business"></Friend>
+        <Friend name={besties[1]} age="35" profession="student"></Friend>
+        <Friend name={besties[2]} age="45" profession="teacher"></Friend>
+        <Friend name={besties[3]} age="55" profession="programmer"></Friend>
+        <Friend name={besties[4]} age="39" profession="mechanic"></Friend>
+      </div >
+    );
+  */
+
+
+  // Example with an Ul and li how this should be done.....
+  /*
+    const besties = ['nabil', 'tushar', 'tamjid', 'shahid', 'sifat'];
+  
+    return (
+      <div className="App">
+        <ul>
+          {
+            // besties.map(besty => console.log(besty))
+            // besties.map(besty => besty)
+            besties.map(besty => <li>{besty}</li>)
+          }
+        </ul>
+        <Friend name={besties[0]} age="29" profession="business"></Friend>
+        <Friend name={besties[1]} age="35" profession="student"></Friend>
+        <Friend name={besties[2]} age="45" profession="teacher"></Friend>
+        <Friend name={besties[3]} age="55" profession="programmer"></Friend>
+        <Friend name={besties[4]} age="39" profession="mechanic"></Friend>
+      </div >
+    );
+    */
+
+
+
+  // Finally how dynamically create multiple components using array of object......
+
+  const besties = ['nabil', 'tushar', 'tamjid', 'shahid', 'sifat'];
+  const billiardMates = [
+    { name: 'nabil', age: 24, strength: 'jump' },
+    { name: 'eifty', age: 12, strength: 'aim' },
+    { name: 'bashar', age: 24, strength: 'bang-kick' },
+    { name: 'sayem', age: 25, strength: 'learner' }
+  ];
+
+  return (
+    <div className="App">
+      <ul>
+        {
+          // besties.map(besty => console.log(besty))
+          // besties.map(besty => besty)
+          besties.map(besty => <li>{besty}</li>)
+        }
+      </ul>
+
+      {/* <PoolBuddy name="eifty" age="24" strength='sfsdf'></PoolBuddy> */}
+
+      {
+        // billiardMates.map(mate => console.log(mate))
+        billiardMates.map(mate => <PoolBuddy name={mate.name} age={mate.age} strength={mate.strength}></PoolBuddy>)
+      }
+
+    </div >
+  );
+}
+
+
+function Friend(props) {
+  console.log(props)
+  const friend = {
+    name: "Nabil",
+    age: 24,
+    profession: 'student'
+  }
+
+  const friendStyles = {
+    backgroundColor: 'lightslategray',
+    color: 'lightgreen',
+    width: '75%',
+    margin: '10px auto',
+    border: '3px solid yellow',
+    borderRadius: '20px'
+  }
+
+
+  return (
+    // <div className='friend'>
+    // <div style={friendStyles}>
+    <div style={friendStyles}>
+      <h1>BestFriend: {props.name}</h1>
+      <h2>Age: {props.age}</h2>
+      <h3>Profession: {props.profession}</h3>
+    </div>
+  )
+}
+
+function PoolBuddy(props) {
+
+  return (
+    <div className="pool-buddy">
+      <h1>Name: {props.name}</h1>
+      <h2>Age: {props.age}</h2>
+      <h3>Strength: {props.strength}</h3>
+    </div>
+  )
+}
+
+function Team(props) {
+
+  return (
+    <div className="team-styles">
+      <h1>Player: {props.name}</h1>
+      <h2>Role: {props.role}</h2>
+      <h3>Strength: {props.strength}</h3>
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
 // Part 3
 
 /*-------------------------------------------------------- ----------------*\
 Dynamic Html to component: Similar in Look Similar in Data, Props in React
 \*---------------------------------------------------------- -----------------*/
-
+/*
 function App() {
 
   return (
@@ -67,7 +210,7 @@ function Team(props) {
 }
 
 export default App;
-
+*/
 
 // Part 2
 
